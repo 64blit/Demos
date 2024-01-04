cfbf7d4f625f9c9c2304d454ea71d42add1266ed
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-01-03 19:47:47 -0800

    Create SceneManager.js

---
+++ AI CDN - Computer Vision Endpoint & UGC Ruleset/js/src/managers/SceneManager.js
@@ -0,0 +1,21 @@
+// a class that builds and manages adding meshes and moving them around
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+export default class SceneManager
+{
+
+    constructor(scene)
+    {
+        this.scene = scene;
+        this.people = [];
+    }
+
+
+
+}
+
+
+// 1. we get the time of the video
+// 2. we get people at that time
+// 3. we build people and meshes around the people
+// 4. 
