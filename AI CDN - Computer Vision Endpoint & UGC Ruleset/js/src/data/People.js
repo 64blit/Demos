73631b9a0b177b406ff4e07a3dadabd52952723e
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-01-03 19:47:36 -0800

    Create People.js

---
+++ AI CDN - Computer Vision Endpoint & UGC Ruleset/js/src/data/People.js
@@ -0,0 +1,17 @@
+// a class to store the people data such as traceID, keypoints like nose, eyes, ears, etc., bounding box, and position in the scene
+
+
+export default class People
+{
+
+    constructor()
+    {
+        this.traceID = null;
+        this.keypoints = null;
+        this.boundingBox = null;
+        this.position = null;
+    }
+
+
+
+}
