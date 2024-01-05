53e7697df2dc4546f8438a6135a57ae4fd4b3602
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-01-04 19:37:21 -0800

    Added cleaner box drawing and center points. Added pose code that needs to be updated

--- AI CDN - Computer Vision Endpoint & UGC Ruleset/js/src/managers/SceneManager.js
+++ AI CDN - Computer Vision Endpoint & UGC Ruleset/js/src/managers/SceneManager.js
@@ -1,7 +1,6 @@
 // a class that builds and manages adding meshes and moving them around
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import PeopleManager from './PeopleManager.js';
-import People from '../data/People.js';
 
 export default class SceneManager
 {
@@ -13,19 +12,6 @@ export default class SceneManager
         this.dimensions = dimensions;
         this.peopleManager = new PeopleManager(dimensions);
         this.activePeople = [];
-
-        // Create an instanced mesh outside of the drawPoint function
-        let instanceGeometry = new THREE.BoxGeometry(.1, .1, 1);
-        let instanceMaterial = new THREE.MeshNormalMaterial({ color: 0xff0000 });
-        instanceGeometry.computeVertexNormals();
-
-        this.mockObject = new THREE.Object3D();
-        this.instanceCount = 10000; // Set this to the maximum number of instances you expect to need
-        this.instanceMesh = new THREE.InstancedMesh(instanceGeometry, instanceMaterial, this.instanceCount);
-
-        console.log("this.instanceMesh: ", this.instanceMesh)
-        // this.instanceMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
-        this.scene.add(this.instanceMesh);
 
     }
 
@@ -45,10 +31,18 @@ export default class SceneManager
                 this.activePeople.push(person);
 
                 this.drawPoint(person);
+                this.drawPath(person);
                 this.drawBoundingBox(person);
+                this.drawPose(person);
             }
         }
 
+        this.hideInactivePeople(previousActivePeople);
+
+    }
+
+    hideInactivePeople(previousActivePeople)
+    {
         // hide people that are no longer in the scene
         for (let i = 0; i < previousActivePeople.length; i++)
         {
@@ -57,17 +51,14 @@ export default class SceneManager
             {
                 person.centerSphere && (person.centerSphere.visible = false);
                 person.pathLine && (person.pathLine.visible = false);
-                person.bounds && (person.bounds.visible = false);
+                person.boundsBox && (person.boundsBox.visible = false);
             } else
             {
                 person.centerSphere && (person.centerSphere.visible = true);
                 person.pathLine && (person.pathLine.visible = true);
-                person.bounds && (person.bounds.visible = true);
+                person.boundsBox && (person.boundsBox.visible = true);
             }
         }
-
-
-        this.drawPaths();
     }
 
     drawPoint(person)
@@ -78,8 +69,8 @@ export default class SceneManager
 
         if (!sphere)
         {
-            let geometry = new THREE.SphereGeometry(0.015, 8, 8);
-            let material = new THREE.MeshNormalMaterial();
+            let geometry = new THREE.SphereGeometry(0.01, 8, 8);
+            let material = new THREE.MeshBasicMaterial({ color: 0x00fa00, transparent: true, opacity: 0.8 });
             sphere = new THREE.Mesh(geometry, material);
 
             this.scene.add(sphere);
@@ -89,8 +80,8 @@ export default class SceneManager
         }
 
         let point = person.position;
-        sphere.position.x = point.y;
-        sphere.position.y = point.x;
+        sphere.position.x = point.x;
+        sphere.position.y = point.y;
         sphere.position.z = 0;
 
     }
@@ -104,62 +95,200 @@ export default class SceneManager
         let line = person.pathLine;
 
         const pathGeometry = new THREE.BufferGeometry().setFromPoints(path);
+
         if (!line)
         {
-            const pathMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: .5, side: THREE.DoubleSide, wireframe: true });
+            const pathMaterial = new THREE.LineBasicMaterial({
+                color: 0xfaa61a,
+                linewidth: 10,
+            });
 
-            person.pathLine = new THREE.Mesh(pathGeometry, pathMaterial);
+            person.pathLine = new THREE.Line(pathGeometry, pathMaterial);
 
             this.scene.add(person.pathLine);
         }
         else 
         {
-            person.pathLine.geometry.copy(pathGeometry);
+            line.geometry.copy(pathGeometry);
+            line.geometry.verticesNeedUpdate = true;
         }
     }
 
 
-
+    // TODO: Draw a sexier bounding box with a sprite or shader
     drawBoundingBox(person)
     {
+        const bounds = person.bounds;
+        if (!bounds) return;
 
-        // draw a sphere at these positions: 
-        //  person.position, person.position.x + person.size.x, person.position.y + person.size.y
+        const boundsBox = person.boundsBox;
 
+        let width = bounds.max.x - bounds.min.x;
+        let height = bounds.max.y - bounds.min.y;
 
-        // let sphere = person.topLeft;
+        const boxGeometry = new THREE.BoxGeometry(width, height, 0);
+        const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, opacity: 0.5, transparent: true });
+        const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
-        // if (!sphere)
-        // {
-        //     let geometry = new THREE.BoxGeometry(0.025, 0.025, 0.025);
-        //     let material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.5 });
-        //     sphere = new THREE.Mesh(geometry, material);
-        //     this.scene.add(sphere);
-        //     person.midPoint = sphere;
-        // }
-        // let point = person.position;
-        // sphere.position.x = point.y;
-        // sphere.position.y = point.x;
-        // sphere.position.z = 0;
+        boxMesh.position.x = person.position.x;
+        boxMesh.position.y = person.position.y;
+        boxMesh.position.z = 0;
 
-    }
-
-    drawPaths()
-    {
-
-        for (let i = 0; i < this.peopleManager.allPeoplePositions.length - 1; i++)
+        if (!boundsBox)
         {
-            let point = this.peopleManager.allPeoplePositions[ i ];
+            this.scene.add(boxMesh);
+            person.boundsBox = boxMesh;
+        }
+        else 
+        {
+            boundsBox.geometry = boxGeometry;
+            boundsBox.material = boxMaterial;
+            boundsBox.position.x = boxMesh.position.x;
+            boundsBox.position.y = boxMesh.position.y;
+            boundsBox.position.z = boxMesh.position.z;
 
-            this.mockObject.position.set(point.x, point.y, 0);
-            this.mockObject.updateMatrix();
-
-            this.instanceMesh.setMatrixAt(i, this.mockObject.matrix);
-
-            this.instanceMesh.instanceMatrix.needsUpdate = true;
+            boundsBox.geometry.verticesNeedUpdate = true;
         }
 
     }
+
+
+    drawPose(person)
+    {
+        const pose = person.pose;
+    }
+
+    //     var points = keyPoints.points
+    //     var canvas2 = VideoPlayer.context.canvas
+    //     var context = canvas2.getContext('2d')
+
+    //     var labelPoints = []
+    //     let fillColor = VideoPlayer.colors.primary_color;
+    //     let max_z = 0;
+    //     let min_z = 0;
+    //     for (var i = 0; i < points.length; i++)
+    //     {
+    //         var p = { ...points[ i ] }
+    //         //normalize point
+    //         p.y_normalized = p.y / source_height
+    //         p.x_normalized = p.x / source_width
+    //         p.z_normalized = p.z / source_width
+
+    //         p.y = p.y_normalized * VideoPlayer.instance.displayHeight
+    //         p.x = p.x_normalized * VideoPlayer.instance.displayWidth
+    //         p.z = p.z_normalized * VideoPlayer.instance.displayWidth
+
+    //         if (p.z > max_z)
+    //         {
+    //             max_z = p.z;
+    //         }
+    //         if (p.z < min_z)
+    //         {
+    //             min_z = p.z;
+    //         }
+
+    //         labelPoints[ p.classLabel ] = p
+    //     }
+
+    //     var connections = [
+    //         [ 'mouth (right)', 'mouth (left)' ],
+    //         [ 'right ear', 'right eye (outer)' ],
+    //         [ 'right eye (outer)', 'right eye' ],
+    //         [ 'right eye', 'right eye (inner)' ],
+    //         [ 'right eye (inner)', 'nose' ],
+    //         [ 'nose', 'left eye (inner)' ],
+    //         [ 'left eye (inner)', 'left eye' ],
+    //         [ 'left eye', 'left eye (outer)' ],
+    //         [ 'left eye (outer)', 'left ear' ],
+
+    //         [ 'right shoulder', 'left shoulder' ],
+    //         [ 'left shoulder', 'left hip' ],
+    //         [ 'left hip', 'right hip' ],
+    //         [ 'right hip', 'right shoulder' ],
+
+    //         [ 'right shoulder', 'right elbow' ],
+    //         [ 'right elbow', 'right wrist' ],
+    //         [ 'right wrist', 'right thumb' ],
+    //         [ 'right wrist', 'right pinky' ],
+    //         [ 'right wrist', 'right index' ],
+    //         [ 'right pinky', 'right index' ],
+
+    //         [ 'left shoulder', 'left elbow' ],
+    //         [ 'left elbow', 'left wrist' ],
+    //         [ 'left wrist', 'left thumb' ],
+    //         [ 'left wrist', 'left pinky' ],
+    //         [ 'left wrist', 'left index' ],
+    //         [ 'left pinky', 'left index' ],
+
+    //         [ 'right hip', 'right knee' ],
+    //         [ 'right knee', 'right ankle' ],
+    //         [ 'right ankle', 'right foot index' ],
+    //         [ 'right ankle', 'right heel' ],
+    //         [ 'right heel', 'right foot index' ],
+
+    //         [ 'left hip', 'left knee' ],
+    //         [ 'left knee', 'left ankle' ],
+    //         [ 'left ankle', 'left foot index' ],
+    //         [ 'left ankle', 'left heel' ],
+    //         [ 'left heel', 'left foot index' ],
+    //     ]
+
+    //     for (var i = 0; i < connections.length; i++)
+    //     {
+    //         var point1 = labelPoints[ connections[ i ][ 0 ] ]
+    //         var point2 = labelPoints[ connections[ i ][ 1 ] ]
+
+    //         if (!point1 || !point1.x) continue
+
+    //         if (!point2 || !point2.x) continue
+
+    //         context.beginPath()
+    //         context.lineWidth = 3
+    //         context.strokeStyle = VideoPlayer.colors.primary_color
+    //         context.fillStyle = VideoPlayer.colors.primary_color
+    //         context.moveTo(point1.x, point1.y)
+    //         context.lineTo(point2.x, point2.y)
+    //         context.stroke()
+    //         context.closePath()
+    //     }
+
+    //     const MIN_RADIUS = 3;
+    //     const MAX_RADIUS = pose.width / 30;
+
+    //     for (let i = 0; i < points.length; i++)
+    //     {
+    //         let p = { ...points[ i ] }
+    //         //normalize pointl
+    //         p.y_normalized = p.y / source_height
+    //         p.x_normalized = p.x / source_width
+    //         p.z_normalized = p.z / source_width
+
+    //         p.y = p.y_normalized * VideoPlayer.instance.displayHeight
+    //         p.x = p.x_normalized * VideoPlayer.instance.displayWidth
+    //         let radius = MAX_RADIUS - (p.z_normalized * VideoPlayer.instance.displayWidth - min_z) * (MAX_RADIUS - MIN_RADIUS) / (max_z - min_z)
+
+    //         //draw circle
+    //         context.beginPath()
+    //         context.arc(p.x, p.y, radius, 0, Math.PI * 2, false)
+    //         if (points[ i ].classLabel.includes('left'))
+    //         {
+    //             context.fillStyle = VideoPlayer.colors.left_color
+    //         } else if (points[ i ].classLabel.includes('right'))
+    //         {
+    //             context.fillStyle = VideoPlayer.colors.right_color
+    //         } else
+    //         {
+    //             context.fillStyle = fillColor
+    //         }
+    //         context.fill()
+    //         context.strokeStyle = VideoPlayer.colors.secondary_color
+    //         context.stroke()
+    //         labelPoints[ p.classLabel ] = p
+    //     }
+    // }
+
+    // }
+
 
 }
 
