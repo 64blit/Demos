96c20229ec168702b938da8ba41cfb8a1f28dec1
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-03-25 12:00:30 -0700

    [cleanup]

--- node/basic-eyepop-example/src/components/ui/UI.tsx
+++ node/basic-eyepop-example/src/components/ui/UI.tsx
@@ -1,6 +1,7 @@
 import { Html } from '@react-three/drei';
 import React from 'react';
 import Dialog from './Dialog';
+import { useSceneStore } from '../../store/SceneStore';
 
 
 const UI: React.FC = () =>
